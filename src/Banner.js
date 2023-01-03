ee72a1e74b0d0c4282cc1783302b83e282a4d53e
Sanjay Thakur <sanjaythakur361899@gmail.com>
2023-01-03 13:58:16 +0530

    Row section completed

--- src/Banner.js
+++ src/Banner.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 const Banner = () => {
-  return <header><div className="banner" style={{backgroundSize:"cover",
-//   backgroundImage:`url("https://i.imgur.com/e1hLQ2m.png)`,
-//   backgroundPosition:"center center"
-  }}>
-     <div className="banner__contents">
-        <h1 className="banner__title">
-          Movie Name
-        </h1>
-        <div className="banner__buttons">
+  const [movie, setMovie] = React.useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      console.log(request,'fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}& with_network=213`,')
+      setMovie(request.data.results[Math.floor(Math.random()* request.data.results.length - 1)])
+    }
+    fetchData();
+  },[]);
+  console.log(movie);
+  return (
+    <header>
+      <div
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+            backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backgroundPosition:"center center"
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+          <div className="banner__buttons">
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">{movie?.overview}</h1>
         </div>
-        <h1 className="banner__description">This is a test description</h1>
-     </div>
-     <div className="banner--fadeBottom"></div>
-  </div>
-  </header>
+        <div className="banner--fadeBottom"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Banner;
