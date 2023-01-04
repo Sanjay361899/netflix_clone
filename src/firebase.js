736bda69c71de8f04c148e04e93781b261ba4cd4
Sanjay Thakur <sanjaythakur361899@gmail.com>
2023-01-04 17:48:35 +0530

    added firebase authorization

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from "firebase/compat/app";
+// import { getFirestore  } from "firebase/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyBucnI03aLEO1rlVll56hRKm_Rx421DcaU",
   authDomain: "netflix-clone-sanjay.firebaseapp.com",
@@ -6,3 +11,10 @@ const firebaseConfig = {
   messagingSenderId: "288481348094",
   appId: "1:288481348094:web:7b30a6db33cb099d4d18af"
 };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+//   const storage= firebaseApp.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+export { db, auth, provider };
+export default firebase;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './SignupScreen.css'
 const SignupScreen = () => {
+    const signIn= (e)=>{
+       e.preventDefault();
+    }
   return (
-    <div>
-      
+    <div className='signupScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='password' />
+        <button onClick={signIn} type='submit'>Sign In</button>
+        <h4><span className='signupScreen__gray'>New to Netflix?</span><span className='signupScreen__link'> Sign Up now.</span></h4>
+      </form>
     </div>
   )
 }
