8a0c8a6ac758d2fc43063d23990ec0965db85c27
Sanjay Thakur <sanjaythakur361899@gmail.com>
2023-01-05 11:27:57 +0530

    firebase authentication with redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginScreen from './screens/LoginScreen';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null
+  const users = useSelector(selectUser);
+  const user = users;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged in
+        console.log(userAuth);
+        dispatch(login({
+          uid:userAuth.uid,
+          email:userAuth.email
+        }))
+      } else {
+        //Logged Out
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
-    <BrowserRouter>
-    <Routes>
-    {!user?<Route path='/' element={<LoginScreen/>}/>  :<Route path='/' element={<HomeScreen/>}/>}
-      </Routes>  </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Routes>
+          {!user ? (
+            <Route path="/" element={<LoginScreen />} />
+          ) : (
+            <Route path="/" element={<HomeScreen />} />
+          )}
+        </Routes>{" "}
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,52 @@
-import React from 'react'
-import './SignupScreen.css'
+import React, { useRef } from "react";
+import "./SignupScreen.css";
+import { auth } from "../firebase";
 const SignupScreen = () => {
-    const signIn= (e)=>{
-       e.preventDefault();
-    }
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser, "authUser");
+      })
+      .catch((error) => alert(error.message));
+  };
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser, "authUser");
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
-    <div className='signupScreen'>
+    <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='password' />
-        <button onClick={signIn} type='submit'>Sign In</button>
-        <h4><span className='signupScreen__gray'>New to Netflix?</span><span className='signupScreen__link'> Sign Up now.</span></h4>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="password" />
+        <button onClick={signIn} type="submit">
+          Sign In
+        </button>
+        <h4>
+          <span className="signupScreen__gray">New to Netflix?</span>
+          <span onClick={register} className="signupScreen__link">
+            {" "}
+            Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignupScreen
+export default SignupScreen;
